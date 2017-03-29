888e154b8dc8c2bebf030c7050ee5bda33bfd092
witnessmenow <ecenuig@gmail.com>
2017-03-29 23:17:01 +0100

    adding dirname to static

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.use('/templates', express.static('templates'))
-app.use('/static', express.static('static'))
+app.use('/templates', express.static(__dirname + '/templates'))
+app.use('/static', express.static(__dirname + '/static'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
